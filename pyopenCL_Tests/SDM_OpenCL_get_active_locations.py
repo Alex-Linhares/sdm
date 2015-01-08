f1d8133863ba901cb7f42df07482400994c2273a
Alexandre Linhares <linhares@clubofrome.org.br>
2015-01-08 06:14:44 -0200

    Refactoring, cleanup, and some other improvements

--- pyopenCL_Tests/SDM_OpenCL_get_active_locations.py
+++ pyopenCL_Tests/SDM_OpenCL_get_active_locations.py
@@ -7,10 +7,34 @@ import time
 
 
 HARD_LOCATIONS = 2**20
-EXPECTED_NUM_HARD_LOCATIONS = 1250
-HASH_TABLE_SIZE =  25033 # 12043 # 25033 #12043  #Must be a prime number in the OpenCL code
+EXPECTED_NUM_HARD_LOCATIONS = 1250  #Compute analytically; prove it's safe... 
 
-# HASH_TABLE_SIZE must be prime.  The higher it is, the more bandwidth, but way less collisions.
+
+
+HASH_TABLE_SIZE =  25033 
+HASH_TABLE_SIZE_FILE = \
+"#define HASH_TABLE_SIZE 25033 \n\
+#define HASH_TABLE_SIZE2 25032 \n\
+#define HASH_TABLE_SIZE3 25031 \n\
+#define HASH_TABLE_SIZE4 25030 \n\
+#define HASH_TABLE_SIZE5 25029 \n\
+#define HASH_TABLE_SIZE6 25028 \n\
+#define HASH_TABLE_SIZE7 25027 \n\
+"
+
+HASH_TABLE_SIZE =  1489 
+HASH_TABLE_SIZE_FILE = \
+"#define HASH_TABLE_SIZE 1489 \n\
+#define HASH_TABLE_SIZE2 1488 \n\
+#define HASH_TABLE_SIZE3 1487 \n\
+#define HASH_TABLE_SIZE4 1486 \n\
+#define HASH_TABLE_SIZE5 1485 \n\
+#define HASH_TABLE_SIZE6 1484 \n\
+#define HASH_TABLE_SIZE7 1483 \n\
+"
+
+
+# HASH_TABLE_SIZE must be prime.  The higher it is, the more bandwidth, but way less collisions.  It should also be "far" from a power of 2.
 
 
 print "HASH_TABLE_SIZE=", HASH_TABLE_SIZE  #WHAT IS THE OPTIMUM HASH_TABLE_SIZE??
@@ -32,8 +56,8 @@ def Get_Hash_Table():
 
 def Get_Hash_Table_GPU_Buffer(ctx):
 	hash_table_active_index = Get_Hash_Table()
-	bin_active_index_gpu = cl.Buffer(ctx, mem_flags.READ_WRITE | mem_flags.COPY_HOST_PTR, hostbuf=hash_table_active_index)
-	return bin_active_index_gpu
+	hash_table_gpu = cl.Buffer(ctx, mem_flags.READ_WRITE | mem_flags.COPY_HOST_PTR, hostbuf=hash_table_active_index)
+	return hash_table_gpu
 
 def Get_Hamming_Distances():
 	hamming_distances = numpy.zeros(HARD_LOCATIONS).astype(numpy.uint32) 
@@ -66,40 +90,24 @@ def Get_Memory_Addresses_Buffer(ctx):
 def Get_Text_code(filename):
 	with open (filename, "r") as myfile:
 	    data = myfile.read()
-	    return data
+	    return HASH_TABLE_SIZE_FILE + data 
 
 
-def Get_Active_Locations(bitstring, ctx):
-	bitstring_gpu = Get_Bitstring_GPU_Buffer(ctx)
-	
-	err = prg.clear_bin_active_indexes_gpu(queue, (HASH_TABLE_SIZE,), None, bin_active_index_gpu).wait()
-
-	err = prg.get_active_hard_locations(queue, (HARD_LOCATIONS,), None, memory_addresses_gpu, bitstring_gpu, distances_gpu, bin_active_index_gpu).wait()  
-	if err: print 'Error --> ',err
-
-	
-	hash_table_active_index = Get_Hash_Table() #THIS IS SLOWING EVERYTHING!
-
-
-	err = cl.enqueue_read_buffer(queue, bin_active_index_gpu, hash_table_active_index).wait()
-	if err: print 'Error in retrieving hash_table_active_index? --> ',err
-
-	# Removing zeros from hash_table_active_index, 2 options:
-	# option 1: use numpy masked arrays
-	# hash_table_active_index = numpy.ma.masked_equal(hash_table_active_index,0).compressed()
-	# option 2: a[a != 0]
-	hash_table_active_index = hash_table_active_index[hash_table_active_index!=0]
-	return hash_table_active_index
-
-
-
-
-#bin_active_index_gpu = Get_Bin_Active_Indexes_GPU_Buffer(ctx)
-
-bin_active_index_gpu = Get_Hash_Table_GPU_Buffer(ctx)
-
+hash_table_gpu = Get_Hash_Table_GPU_Buffer(ctx)
 memory_addresses_gpu = Get_Memory_Addresses_Buffer(ctx)
 distances_gpu = Get_Distances_GPU_Buffer(ctx)
+
+def Get_Active_Locations(bitstring, ctx):
+	err = prg.clear_hash_table_gpu(queue, (HASH_TABLE_SIZE,), None, hash_table_gpu).wait()
+	#err = prg.get_active_hard_locations(queue, (HARD_LOCATIONS,), None, memory_addresses_gpu, bitstring_gpu, distances_gpu, hash_table_gpu).wait()  
+	err = prg.get_active_hard_locations_no_dist_buffer(queue, (HARD_LOCATIONS,), None, memory_addresses_gpu, bitstring_gpu, hash_table_gpu).wait()  
+	if err: print 'Error --> ',err
+	err = cl.enqueue_read_buffer(queue, hash_table_gpu, hash_table_active_index).wait()
+	if err: print 'Error in retrieving hash_table_active_index? --> ',err
+
+	active_hard_locations = hash_table_active_index[hash_table_active_index!=0]  ## THIS HAS TO BE DONE ON THE GPU, MORON!  If it's sorted, you can get the num of items and copy only those...
+	return active_hard_locations
+
 
 
 
@@ -118,59 +126,32 @@ hamming_distances = Get_Hamming_Distances()
 
 print "\n"
 
-Results = numpy.zeros(HASH_TABLE_SIZE).astype(numpy.uint32) 
+num_times = 2000
+Results_and_Statistics = numpy.zeros(num_times+1).astype(numpy.uint32) 
 
 start = time.time()
-num_times = 2000
 for x in range(num_times):
-	
 	bitstring_gpu = Get_Bitstring_GPU_Buffer(ctx)
+	active_hard_locations = Get_Active_Locations(bitstring_gpu, ctx)
+	Results_and_Statistics[x] = numpy.size(active_hard_locations)
 	
-	err = prg.clear_bin_active_indexes_gpu(queue, (HASH_TABLE_SIZE,), None, bin_active_index_gpu).wait()
-
-	err = prg.get_active_hard_locations(queue, (HARD_LOCATIONS,), None, memory_addresses_gpu, bitstring_gpu, distances_gpu, bin_active_index_gpu).wait()  
-	if err: print 'Error --> ',err
-
-	
-	hash_table_active_index = Get_Hash_Table() #THIS IS SLOWING EVERYTHING!
-
-
-	err = cl.enqueue_read_buffer(queue, bin_active_index_gpu, hash_table_active_index).wait()
-	if err: print 'Error in retrieving hash_table_active_index? --> ',err
-
-	# Removing zeros from hash_table_active_index, 2 options:
-	# option 1: use numpy masked arrays
-	# hash_table_active_index = numpy.ma.masked_equal(hash_table_active_index,0).compressed()
-	# option 2: a[a != 0]
-	hash_table_active_index = hash_table_active_index[hash_table_active_index!=0]
-	
-
-	#print hash_table_active_index
-	num_active_locations_found = numpy.size(hash_table_active_index)
-	#print  "num_active_locations_found=", num_active_locations_found
-	Results[x] = num_active_locations_found
-	
-	time_elapsed = (time.time()-start)
-	#if (x%1==0): print x, time_elapsed, "\n\n"
-
 time_elapsed = (time.time()-start)
 
-mean  = Results[Results !=0].mean()
 
-print Results[Results !=0].min(), " the minimum of HLs found should be 1001"
-print mean, "the mean of HLs found should be 1119.077"
-print Results[Results !=0].max(), "the max of HLs found should be 1249"
+print Results_and_Statistics[Results_and_Statistics !=0].min(), " the minimum of HLs found should be 1001"
+print Results_and_Statistics[Results_and_Statistics !=0].mean(), "the mean of HLs found should be 1119.077"
+print Results_and_Statistics[Results_and_Statistics !=0].max(), "the max of HLs found should be 1249"
+
+print numpy.size(active_hard_locations)
+print active_hard_locations
 
 
-print numpy.size(hash_table_active_index)
+#print hamming_distances[hash_table_active_index[hash_table_active_index!=0]]
+#HEY!  You've never got those distances back... not even saved them in GPU memory... moron...
 
-print hash_table_active_index
+print '\n Seconds to Scan 2^20 Hard Locations', num_times,'times:', time_elapsed
 
-print hamming_distances[hash_table_active_index]
-
-print '\nTime to compute some Hamming distances', num_times,'times:', time_elapsed
-
-sum = numpy.sum(Results)
+sum = numpy.sum(Results_and_Statistics)
 
 
 # multiple-hashing in OpenCL code
@@ -178,27 +159,3 @@ usual_result = 2238155
 print '\n Sum of num_active_locations_found locations = ', sum, "error =", sum-usual_result, "\n"
 print 'expected HLs missed per scan is', (usual_result-sum)/num_times
 
-
-
-
-# RETRIEVE num_ACTIVE_locations_found HARDLOCATIONS!
-#==================================
-'''
-from pyopencl.scan import GenericScanKernel
-
-knl = GenericScanKernel(
-        ctx, numpy.int32,
-        arguments="__global int *ary, __global int *out",
-        input_expr="(ary[i] < 104) ? 1 : 0",
-        scan_expr="a+b", neutral="0",
-        output_statement="""if (prev_item != item) out[item-1] = ary[i];""")
-
-#ary = hamming_distances.copy()
-out = hamming_distances.copy()
-knl(hamming_distances, out).wait()
-
-a_host = ary.get()
-out_host = a_host[a_host < 104]
-
-assert (out_host == out.get()[:len(out_host)]).all()
-'''
