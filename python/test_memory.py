b971dbd3f50040854316fd58a897e9a759093a86
Alexandre Linhares <linhares@clubofrome.org.br>
2015-02-06 02:48:15 -0200

    Merge branch 'alex_old_code' of https://github.com/msbrogli/sdm
    Conflicts:
    Makefile
    python/sdm.py
    python/test_memory.py
    sdm/common.h
    sdm/memory_opencl_program.cl
    src/core/bitstring.c
    src/core/memory_thread.c

--- python/test_memory.py
+++ python/test_memory.py
@@ -12,6 +12,7 @@ class MemoryTestCase(unittest.TestCase):
         p = subprocess.Popen("ps -p %d -o rss | grep '^ *[0-9]\+ *$'" % os.getpid(), shell=True, stdout=subprocess.PIPE)
         return int(p.communicate()[0])
 
+<<<<<<< HEAD
     #def test_saveload(self):
     #    a = Bitstring()
     #    sdm.initialize()
@@ -33,12 +34,39 @@ class MemoryTestCase(unittest.TestCase):
     #        sdm.free()
     #    m1 = self._memused()
     #    self.assertTrue(m1 < 40000)
+=======
+    def test_saveload(self):
+        a = Bitstring()
+        sdm.initialize()
+        sdm.thread_write(a, a)
+        self.assertEqual(a.distance_to(sdm.thread_read(a)), 0)
+        self.assertEqual(sdm.save_to_file('_test.sdm'), 0)
+        sdm.free()
+        sdm.initialize()
+        self.assertTrue(a.distance_to(sdm.read(a)) > 0)
+        sdm.free()
+        self.assertEqual(sdm.initialize_from_file('_test.sdm'), 0)
+        self.assertEqual(a.distance_to(sdm.thread_read(a)), 0)
+        sdm.free()
+
+    def test_initialize_free(self, qty=5):
+        m0 = self._memused()
+        for i in range(qty):
+            sdm.initialize()
+            sdm.free()
+        m1 = self._memused()
+        self.assertTrue(m1 < 40000)
+>>>>>>> FETCH_HEAD
 
     def test_mean_distance(self, qty=10):
         sdm.initialize()
         for i in range(qty):
             a = Bitstring()
+<<<<<<< HEAD
             arr = array(sdm.thread_distance(a))
+=======
+            arr = array(sdm.distance(a))
+>>>>>>> FETCH_HEAD
             self.assertTrue(abs(arr.mean()-500) <= 1.5)
         sdm.free()
 
@@ -62,6 +90,29 @@ class MemoryTestCase(unittest.TestCase):
             self.assertEqual(a.distance_to(c), 0)
         sdm.free()
 
+<<<<<<< HEAD
+=======
+class MemoryThreadTestCase(unittest.TestCase):
+
+    def test_distance(self, qty=10):
+        sdm.initialize()
+        for i in range(qty):
+            a = Bitstring()
+            d1 = sdm.distance(a)
+            d2 = sdm.thread_distance(a)
+            self.assertEqual(d1, d2)
+        sdm.free()
+
+    def test_radius_count(self, qty=10):
+        sdm.initialize()
+        for i in range(qty):
+            a = Bitstring()
+            cnt1 = sdm.radius_count(a)
+            cnt2 = sdm.thread_radius_count(a)
+            self.assertEqual(cnt1, cnt2)
+        sdm.free()
+
+>>>>>>> FETCH_HEAD
     def test_radius_count_intersect(self, qty=10):
         sdm.initialize()
         for i in range(qty):
@@ -71,6 +122,36 @@ class MemoryTestCase(unittest.TestCase):
             cnt2 = sdm.thread_radius_count_intersect(a, b)
             self.assertEqual(cnt1, cnt2)
         sdm.free()
+<<<<<<< HEAD
+=======
+
+    def test_writeread1(self, qty=20):
+        sdm.initialize()
+        for i in range(qty):
+            a = Bitstring()
+            sdm.thread_write(a, a)
+            b = sdm.read(a)
+            self.assertEqual(a.distance_to(b), 0)
+        sdm.free()
+
+    def test_writeread2(self, qty=10):
+        sdm.initialize()
+        for i in range(qty):
+            a = Bitstring()
+            sdm.write(a, a)
+            b = sdm.thread_read(a)
+            self.assertEqual(a.distance_to(b), 0)
+        sdm.free()
+
+    def test_writeread3(self, qty=10):
+        sdm.initialize()
+        for i in range(qty):
+            a = Bitstring()
+            sdm.thread_write(a, a)
+            b = sdm.thread_read(a)
+            self.assertEqual(a.distance_to(b), 0)
+        sdm.free()
+>>>>>>> FETCH_HEAD
 
 
 
